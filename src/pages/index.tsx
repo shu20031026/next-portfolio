7ce7842fb8435e5de7104048beb26211ad616347
shu20031026 <shu20031026@gmail.com>
2022-04-13 22:29:34 +0900

    emotionの導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
-import type { NextPage } from 'next';
+import React from "react";
+import type { NextPage } from "next";
+import { css } from "@emotion/react";
+
+const name = css`
+  color: red;
+`;
 
 const Home: NextPage = () => {
   return (
     <>
-      <div>
-        shu20031026
-      </div>
+      <div css={name}>shu20031026</div>
     </>
   );
 };
