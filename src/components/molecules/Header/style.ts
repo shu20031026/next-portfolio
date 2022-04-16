72c9e8a1bbc75b7c76124ba18518d8fd8dc9bf97
shu20031026 <shu20031026@gmail.com>
2022-04-16 23:45:51 +0900

    add: breakポイントの設定

--- src/components/molecules/Header/style.ts
+++ src/components/molecules/Header/style.ts
@@ -1,16 +1,18 @@
 import { css } from "@emotion/react";
 import { palette } from "/libs/color";
+import { breakpoints } from "/libs/screenSize";
 
 export const headerContainer = css`
   width: 100%;
-  height: 40px;
+  height: 44px;
   background-color: ${palette.secondary.main};
   display: flex;
+  justify-content: center;
   align-items: center;
 `;
 
 export const headerWrapper = css`
-  width:100%;
+  width: 1000px;
   color: ${palette.text.dark.default};
   display: flex;
   justify-content: space-around;
@@ -21,7 +23,11 @@ export const nameContents = css`
 `;
 
 export const headerContents = css`
-  font-size: 1.4rem;
+  @media (max-width: ${breakpoints.pc}px){
+    display: none;
+  }
+  font-size: 1.0rem;
+  padding-top: 6px; 
   &:hover{
     color: ${palette.text.dark.hover};
   };
