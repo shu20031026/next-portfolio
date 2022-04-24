83675acd821295e0c93b7bbf3c82058c27bd53a7
shu20031026 <shu20031026@gmail.com>
2022-04-24 22:26:24 +0900

    add: layoutの作成

--- src/components/domain/Home/index.tsx
+++ src/components/domain/Home/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "~/components/molecules/Header";
 import { home } from "./style";
 
 const HomePage = () => {
   return (
     <>
-      <Header/>
       <div css={home}>
         home 
       </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 import { RecoilRoot } from 'recoil';
+import { Layout } from '~/layouts';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <RecoilRoot>
       <NextUIProvider>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </NextUIProvider>
     </RecoilRoot>
   );
