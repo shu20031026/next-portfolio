2a3db70a51bdf8ee79945370fbdd31d1937485ea
shu20031026 <shu20031026@gmail.com>
2022-05-01 22:26:22 +0900

    fix: footerのheightを調整

--- src/components/molecules/Footer/style.ts
+++ src/components/molecules/Footer/style.ts
@@ -5,7 +5,7 @@ import { rootZIndex } from "/libs/zIndex";
 export const footerContainer = css`
   background-color: ${palette.secondary.main};
   width: 100%;
-  height: 5.5vh;
+  height: 10vh;
   display: flex;
   justify-content: center;
   align-items: center;
