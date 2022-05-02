2a907926220c1f3a5291b2c0481fa3774fb5933c
shu20031026 <shu20031026@gmail.com>
2022-05-02 23:26:57 +0900

    add: cmsからの型定義

---
+++ types/works.d.ts
@@ -0,0 +1,41 @@
+export type Works = {
+  "contents": Work[],
+  "totalCount": number,
+  "offset": number,
+  "limit": number,
+}
+
+export type Work = {
+  "id": string,
+  "createdAt": string,
+  "updatedAt": string,
+  "publishedAt": string,
+  "revisedAt": string,
+  "title": string,
+  "content": string,
+  "image":{
+    "url": string,
+    "height": number,
+    "width": number,
+  },
+  "tech": Tech[],
+  "github": string,
+  "link": string
+}
+
+export type Tech = {
+  "id": string,
+  "createdAt": string,
+  "updatedAt": string,
+  "publishedAt": string,
+  "revisedAt": string,
+  "name": string,
+  "category":string[]
+}
+
+export type Techs = {
+  "contents": Tech[],
+  "totalCount": number,
+  "offset": number,
+  "limit": number,
+}
