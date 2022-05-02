8631fe86622041c93563787e6526125025ef9544
shu20031026 <shu20031026@gmail.com>
2022-05-03 00:19:40 +0900

    fix: typeの記法の修正

--- types/works.d.ts
+++ types/works.d.ts
@@ -1,41 +1,41 @@
 export type Works = {
-  "contents": Work[],
-  "totalCount": number,
-  "offset": number,
-  "limit": number,
+  contents: Work[];
+  totalCount: number;
+  offset: number;
+  limit: number;
 }
 
 export type Work = {
-  "id": string,
-  "createdAt": string,
-  "updatedAt": string,
-  "publishedAt": string,
-  "revisedAt": string,
-  "title": string,
-  "content": string,
-  "image":{
-    "url": string,
-    "height": number,
-    "width": number,
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  image:{
+    url: string;
+    height: number;
+    width: number;
   },
-  "tech": Tech[],
-  "github": string,
-  "link": string
+  tech: Tech[];
+  github: string;
+  link: string;
 }
 
 export type Tech = {
-  "id": string,
-  "createdAt": string,
-  "updatedAt": string,
-  "publishedAt": string,
-  "revisedAt": string,
-  "name": string,
-  "category":string[]
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+  category:string[];
 }
 
 export type Techs = {
-  "contents": Tech[],
-  "totalCount": number,
-  "offset": number,
-  "limit": number,
+  contents: Tech[];
+  totalCount: number;
+  offset: number;
+  limit: number;
 }
