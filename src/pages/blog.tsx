5a2abe74e64b4e6cb70cf628015c090472c79c42
shu20031026 <shu20031026@gmail.com>
2022-05-03 22:33:05 +0900

    add: blogページの作成

---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { NextPage } from "next";
+
+const Blog: NextPage = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Blog;
