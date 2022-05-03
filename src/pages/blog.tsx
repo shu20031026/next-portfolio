6b219fd77a4c5b2699c675bc153ed45841052984
shu20031026 <shu20031026@gmail.com>
2022-05-03 22:35:15 +0900

    add: blogページのコンポーネント作成

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import type { NextPage } from "next";
+import BlogPage from "~/components/domain/Brog";
 
 const Blog: NextPage = () => {
   return (
     <>
-      <div></div>
+      <BlogPage/>
     </>
   );
 };
