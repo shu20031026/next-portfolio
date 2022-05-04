b34258887dd4a1a39c793ea5dc7de275cfa531ae
shu20031026 <shu20031026@gmail.com>
2022-05-05 00:13:01 +0900

    fix: _App.tsxの型定義

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,9 @@ import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 import { RecoilRoot } from 'recoil';
 import { Layout } from '~/layouts';
+import { NextPage } from 'next';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp:NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <RecoilRoot>
       <NextUIProvider>
