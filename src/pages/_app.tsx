ca449b7d843e51c1d0d5a264b14af80570e2d1a4
shu20031026 <shu20031026@gmail.com>
2022-05-05 00:29:45 +0900

    fix: _app.tsxのフォーマット

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { NextUIProvider } from '@nextui-org/react';
-import { RecoilRoot } from 'recoil';
-import { Layout } from '~/layouts';
-import { NextPage } from 'next';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { RecoilRoot } from "recoil";
+import { Layout } from "~/layouts";
+import { NextPage } from "next";
 
-const MyApp:NextPage<AppProps> = ({ Component, pageProps }) => {
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <RecoilRoot>
       <NextUIProvider>
