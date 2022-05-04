f1a0359deb31026c8bdef1dc151d0e81dfcb3e13
shu20031026 <shu20031026@gmail.com>
2022-05-04 23:24:56 +0900

    add: contactページの追加

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { NextPage } from "next";
+import ContactPage from "~/components/domain/Contact";
+
+const Contact: NextPage = () => {
+  return (
+    <>
+      <ContactPage />
+    </>
+  );
+};
+
+export default Contact;
