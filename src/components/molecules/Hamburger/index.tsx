85b8ad1a1d10c7dfd2cae952a370d8b804451c28
shu20031026 <shu20031026@gmail.com>
2022-05-05 01:48:21 +0900

    fix: 未使用のimportの削除

--- src/components/molecules/Hamburger/index.tsx
+++ src/components/molecules/Hamburger/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { useRecoilState } from "recoil";
 import { drawerState } from "~/globalStates/atoms";
 import {
