4622a68ca5d605df7a822334d7eb2bbc3cfab6d6
shu20031026 <shu20031026@gmail.com>
2022-05-05 00:29:12 +0900

    fix: layoutsのフォーマット

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -6,18 +6,16 @@ import PageList from "~/data/pageList.json";
 import Footer from "~/components/molecules/Footer";
 
 type LayoutProps = Required<{
-  readonly children: ReactElement
-}>
+  readonly children: ReactElement;
+}>;
 
-export const Layout: React.VFC<LayoutProps> = ({children}) => {
-  return(
+export const Layout: React.VFC<LayoutProps> = ({ children }) => {
+  return (
     <div>
       <Header pageList={PageList}></Header>
       <div css={bodyContainer}>
-        <DrawerMenu pageList={PageList}/>
-        <div css={mainContent}>
-          {children}
-        </div>
+        <DrawerMenu pageList={PageList} />
+        <div css={mainContent}>{children}</div>
         <Footer />
       </div>
     </div>
