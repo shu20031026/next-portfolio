ff99d18f01d5bc81f6007ddd84b0c7d843660d2e
shu20031026 <shu20031026@gmail.com>
2022-05-10 00:42:47 +0900

    fix: worksページの背景を追加

--- src/components/domain/Works/index.tsx
+++ src/components/domain/Works/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { WorkType } from "/types/works";
 import {work} from "./style";
+import { backgroundWhite } from "/libs/backgroundStyle";
 type Props = {
   works: WorkType[];
 };
@@ -10,13 +11,15 @@ const WorksPage: React.VFC<Props> = ({ works }) => {
   return (
     <>
       <div css={work}>
-        {works.map((work) => {
-          return (
-            <Link key={work.id} href={`works/${work.id}`}>
-              <div>{work.title}</div>
-            </Link>
-          );
-        })}
+        <div css={backgroundWhite}>
+          {works.map((work) => {
+            return (
+              <Link key={work.id} href={`works/${work.id}`}>
+                <div>{work.title}</div>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </>
   );
