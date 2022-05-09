d02eb81c7ef09fdf043155d4c41f817e982990a6
shu20031026 <shu20031026@gmail.com>
2022-05-09 23:14:35 +0900

    fix: fetchWorksDataのフォーマット

--- src/apis/microCMS/works.ts
+++ src/apis/microCMS/works.ts
@@ -1,8 +1,8 @@
 import { client } from "/libs/cmsClient";
 import { WorkType } from "/types/works";
 
-export const fetchWorksData = async ():Promise<WorkType[]> => {
-  const fetchData = await client.get({endpoint: "works"});
+export const fetchWorksData = async (): Promise<WorkType[]> => {
+  const fetchData = await client.get({ endpoint: "works" });
   const works = await fetchData.contents;
   return works;
 };
